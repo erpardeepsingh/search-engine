e8a592f1310b5f90307704fce5f31a31e420be68
erpardeepsingh <itspardeepsingh@gmail.com>
2019-12-16 18:46:13 +0530

    ranking

--- functions/search.js
+++ functions/search.js
@@ -3,7 +3,7 @@ const pool = require('../lib/db');
 function search(search_string, func) {
 	str = search_string.replace(/\s+/g, " | ");
 	 pool.query(
-	 	"SELECT * FROM apis WHERE fts @@ to_tsquery($1)", 
+	 	"SELECT api_name,vendor_name, ts_rank_cd(weighted_tsv, to_tsquery($1)) AS rank FROM apis WHERE weighted_tsv @@ to_tsquery($1) ORDER BY rank DESC", 
 	 	[str],
 	 	function(err, result) {	 
 		    if(err) {
